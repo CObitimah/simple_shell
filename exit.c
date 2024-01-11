5683f01b0bd4a1afd94a3b36e477b6db44433bc4
CObitimah <obitimahcephas@gmail.com>
2024-01-10 17:29:31 -0800

    exit

---
+++ exit.c
@@ -0,0 +1,48 @@
+#include "shell.h"
+
+/**
+ * free_array - frees an array
+ * @args: pointer array
+ */
+void free_array(char **args)
+{
+	int a;
+
+	if (args)
+	{
+		for (a = 0; ags[a]; a++)
+			free(args[a]);
+		free(args);
+	}
+}
+
+/**
+ * Exit_Command - function to handle the exit builtin command
+ * @cmd: command input by the user
+ * @line: command input buffer
+ * @status: command status
+ * Return: exit status
+ */
+int Exit_Command(char **cmd, char *line, int status)
+{
+	int exitStatus, a;
+
+	if (cmd[1] == NULL)
+	{
+		free_array(cmd);
+		free(line);
+		exit(status);
+	}
+	for (a = 0; cmd[1][a]; a++)
+	{
+		if (isdigit(cmd[1][a]) == 0)
+		{
+			free_array(cmd);
+			return (1);
+		}
+	}
+	exitStatus = _atoi(cmd[1]);
+	free_array(cmd);
+	free(line);
+	exit(exitStatus);
+}
